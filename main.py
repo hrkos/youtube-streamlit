6b3ff4892e799df442d05034156aaa192669968d
hrkos <float130awh@gmail.com>
2022-05-20 10:18:00 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from matplotlib import image
-from sympy import ConditionSet
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title('Streamlit 超入門')
